a3981e6f0332f6fb595850f7dc3b56d56b671a99
Natali Mizrahi <natalimizrahi22@gmail.com>
2025-05-13 18:24:29 +0300

    added ui

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import './ProgressBar.css';
+import React from "react";
+import "./ProgressBar.css";
 
 const CoolRedBar = ({ percentage = 70, label = "Progress" }) => {
   return (
-    <div className="bar-container">
-      <div className="bar-label">{label}</div>
-      <div className="bar-background">
-        <div
-          className="bar-fill"
-          style={{ width: `${percentage}%` }}
-        />
+    <div className='bar-container'>
+      <div className='bar-label'>{label}</div>
+      <div className='bar-background'>
+        <div className='bar-fill' style={{ width: `${percentage}%` }} />
+        <div id='full_body' style={{ left: `${percentage}%` }}>
+          <div id='right_arm'></div>
+          <div id='right_leg'></div>
+          <div id='head'></div>
+          <div id='left_leg'></div>
+          <div id='torso'></div>
+          <div id='left_arm'></div>
+        </div>
       </div>
-      <div className="bar-percentage">{percentage}%</div>
+      <div className='bar-percentage'>{percentage}%</div>
     </div>
   );
 };
